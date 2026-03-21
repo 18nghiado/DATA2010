d6b04d385854b386b89a1a88ff249152a50ee383
18nghiado <doducnghia2002@gmail.com>
2026-03-21 17:10:09 -0500

    Reorganize files and update directories

--- src/pipelines/cleaning.py
+++ src/pipelines/cleaning.py
@@ -4,7 +4,8 @@ import os
 import json
 
 # ===== CONFIG =====
-DATA_DIR = "data"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_DIR = os.path.join(BASE_DIR, "data")
 
 RAW_DIR = os.path.join(DATA_DIR, "raw")
 TOKEN_DIR = os.path.join(RAW_DIR, "token_datasets")
